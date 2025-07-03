9dfcad01df4637e211c675e24a486dd396212870
Roberto Reyes <robertoireyes.m@gmail.com>
2025-07-03 00:19:14 -0600

    Catalogo

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface Template {
+    id: number;
+    title: string;
+    cost: number;
+    description: string;
+    imageUrl: string;
+    rating: number;
+    previewLink: string;
+    purchaseLink: string;
+    category: string;
+}
